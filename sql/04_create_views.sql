521f5f1520bd8c312e864e6702524a3285376359
Olexiy Koval <kalex32@ukr.net>
2026-07-29 10:42:18 +0300

    Refactor SQL views into internal and public layers

--- sql/04_create_views.sql
+++ sql/04_create_views.sql
@@ -1,32 +1,75 @@
 -- =====================================================
 -- Project : Nova Poshta Analytics
 -- File    : 04_create_views.sql
--- Purpose : Create business shipment view for Power BI
+-- Purpose : Create business shipment and business sender views for Power BI
 -- Author  : Olexiy Koval
+
+-- NOTE:
+-- Internal views are used by the customer report.
+-- Views with the '_public' suffix expose anonymized data for the public portfolio.
 -- =====================================================
+
+CREATE OR REPLACE VIEW np.v_business_sender AS
+SELECT
+	sender_id,
+    sender_contact,
+    sender_phone,
+    sender_city
+   FROM np.dim_business_sender;
+
+CREATE OR REPLACE VIEW np.v_business_sender_public AS
+SELECT
+	sender_id,
+	'BS' || LPAD(sender_id::text, 3, '0') AS sender_public,
+	sender_city
+FROM np.v_business_sender;
+
 
 CREATE OR REPLACE VIEW np.v_shipments_business AS
 SELECT
-shipment_number,
-created_at,
-received_at,
-sender_contact,
-sender_city,
-sender_address,
-shipment_description,
-receiver_address,
-weight_kg,
-places_count,
-declared_value,
-delivery_cost
-FROM np.shipments_final
+	f.shipment_number,
+	f.created_at,
+	f.received_at,
+	f.sender_contact,
+	s.sender_id,
+	f.sender_city,
+	f.sender_address,
+	f.shipment_description,
+	f.receiver_address,
+	f.weight_kg,
+	f.places_count,
+	f.declared_value,
+	f.delivery_cost
+FROM np.shipments_final f
+JOIN np.dim_business_sender s
+ON f.sender_contact = s.sender_contact
 WHERE shipment_type = 'business';
+
+CREATE OR REPLACE VIEW np.v_shipments_business_public AS
+SELECT
+	'TTN-' || RIGHT(shipment_number, 4) AS shipment_number,
+	created_at,
+	received_at,
+	sender_id,
+	weight_kg,
+	places_count,
+	declared_value,
+	delivery_cost
+FROM np.v_shipments_business;
 
 
 -- ========================================
 -- Validation
 -- ========================================
 
-SELECT 
-count(*) AS v_shipments_business
+SELECT COUNT(*) AS v_business_sender
+FROM np.v_business_sender;
+
+SELECT COUNT(*) AS v_business_sender_public
+FROM np.v_business_sender_public;
+
+SELECT COUNT(*) AS v_shipments_business
 FROM np.v_shipments_business;
+
+SELECT COUNT(*) AS v_shipments_business_public
+FROM np.v_shipments_business_public;
